fbaa52e6ad8bf0430bf1b54b3484a58381484b64
RayhaanT <tanweer.rayhaan@gmail.com>
2024-12-15 15:47:34 -0500

    Comments so I don't forget how it works

--- Day4.hs
+++ Day4.hs
@@ -23,18 +23,24 @@ countSubstr substr s = length $ filter (isPrefixOf substr) (tails s)
 diagonals :: [[a]] -> [[a]]
 diagonals s = map reverse $ diags (replicate (length $ head s) []) s
 
+-- Get a list of diagonals out of a square grid
+-- tmp tracks the list of "live" diagonals
+-- The rightmost one ends at the current line as it falls off the grid
+-- Add current line to corresponding diagonals, pop off last diagonal, keep going
 diags :: [[a]] -> [[a]] -> [[a]]
 diags tmp [] = reverse $ tail tmp
 diags tmp grid = (last s : last tmp) : rec
     where s = head grid
           rec = diags ([] : zipWith (:) (init s) (init tmp)) (tail grid)
 
+-- Extract all 3x3 tiles
 getTiles :: [[a]] -> [[[a]]]
 getTiles grid
     | length grid < 3 = []
     | length (head grid) == 3 = take 3 grid : getTiles (tail grid)
     | otherwise = getTiles (transpose $ take 3 grid) ++ getTiles (tail grid)
 
+-- Check if a 3x3 tile is X-MAS
 countMAS s = fcountSubstr "MAS" s + rcountSubstr "MAS" s
 verifyTile t = (countMAS d + countMAS dl) == 2
     where d = diagonals t
