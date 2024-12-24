96493c2034c1b220bdaf25ade02ffb9393d41ac9
RayhaanT <tanweer.rayhaan@gmail.com>
2024-12-23 21:21:02 -0500

    Done Day 12

--- Day12.hs
+++ Day12.hs
@@ -11,16 +11,6 @@ main = do
         s = S.empty
     print $ bfsAll grid
     print $ allSides grid
-    -- print $ bfs grid S.empty (0,1)
-
-uFind :: M.Map Int Int -> Int -> (Int, M.Map Int Int)
-uFind m n
-    | Just p <- parent = uFind (maybe m (\x -> M.insert n x m) (M.lookup p m)) p
-    | otherwise = (n, m)
-    where parent = M.lookup n m
-
-uUnion :: M.Map Int Int -> Int -> Int -> M.Map Int Int
-uUnion m a b = M.insert a b m
 
 bfsAll :: [String] -> Int
 bfsAll g = fst $ foldl (\(n, s) p -> (\(a,b,c)->(n+length a*b,c)) $ bfs g s p) (0, S.empty) g'
@@ -41,55 +31,30 @@ valid g (x,y) = x >= 0 && y >= 0 && y < length g && x < length (head g)
 same g c (x,y) = c == c' where c' = (g!!y)!!x
 add (a,b) (c,d) = (a+c, b+d)
 
-data Dir = U | D | L | R deriving (Eq, Show)
-right U = R
-right R = D
-right D = L
-right L = U
-left U = L
-left L = D
-left D = R
-left R = U
-delta U = (0,-1)
-delta D = (0,1)
-delta R = (1,0)
-delta L = (-1,0)
-
-add' (a,b,e) (c,d,e') = (a+c,b+d,e')
-sub (a,b) (c,d) = (a-c, b-d)
-
 allSides :: [String] -> Int
 allSides g = fst $ foldl (\(n, s) p -> (\(a,_,c)->(n+nsides a*length a,c)) $ bfs g s p) (0, S.empty) g'
     where g' = [(x,y) | x <- [0..length (head g)-1], y <- [0..length g-1]]
 
-extract (a,b,c) = (a,c)
-
 nsides :: [(Int, Int)] -> Int
-nsides s = fst $ foldl
-    (\(n, set) p -> if p `S.member` set then (n,set) else let (n',set') = walkFrom s p in (n'+n, S.union set set'))
-    (0, S.empty) interest
-    where interest = [ p | p <- s, add p (delta U) `notElem` s ]
+nsides [] = 0
+nsides [a] = 4
+nsides ps = sum (map (corners ps) ps)
 
-eq (a,b,_) (a',b',_) = (a == a') && (b == b')
+corners :: [(Int, Int)] -> (Int, Int) -> Int
+corners ps p = interiorCorners ps p + case length vdeltas + length hdeltas of
+                    1 -> 2
+                    2 -> if length vdeltas == 1 then 1 else 0
+                    _ -> 0
+    where vdeltas = filter (`elem` ps) [add p dp | dp <- [(0,1), (0,-1)]]
+          hdeltas = filter (`elem` ps) [add p dp | dp <- [(1,0), (-1,0)]]
 
-walkFrom :: [(Int, Int)] -> (Int, Int) -> (Int, S.Set (Int, Int))
-walkFrom a b | trace (show b) False = undefined
-walkFrom [] _ = (0, S.empty)
-walkFrom s start = extract $ until (\(i,p,_) -> p `eq` start' && i > 0) (\(i,p,seen) -> stepCount s seen i p) (0, start', S.empty)
-    where start' = uncurry (,,R) start
-    -- (\(x,y)->(x,y,R)) $ until (`notElem` s) (\(x,y) -> (x,y-1)) $ head s
+helper d a b c = fromEnum (d!!a && d!!b && not (d!!c))
 
-stepCount [a] s _ t@(x,y,d) = (4, t, S.insert (x,y) s)
-stepCount region seen i t@(x,y,d)
-  -- | trace (show t ++ " " ++ show i) False = undefined
-  | d' == d = (i,t',seen')
-  | otherwise = (i+1,t',seen')
-  where t'@(_,_,d') = step region t
-        seen' = S.insert (x,y) seen
-
-step :: [(Int, Int)] -> (Int, Int, Dir) -> (Int, Int, Dir)
-step s t@(x,y,d)
-  | (x, y) `notElem` s = add' t $ uncurry ( , , right d) $ add (sub (0,0) $ delta d) $ delta $ right d
-  | rail `elem` s = add' t $ uncurry ( , , left d) $ delta $ left d
-  | otherwise = add' t $ uncurry ( , , d) $ delta d
-  where rail = add (x,y) $ delta $ left d
+interiorCorners :: [(Int, Int)] -> (Int, Int) -> Int
+interiorCorners ps p = helper mdeltas 0 2 1 + helper mdeltas 2 4 3 +
+        helper mdeltas 4 6 5 + helper mdeltas 6 0 7
+    -- 701
+    -- 6.2
+    -- 543
+    where deltas = [add p dp | dp <- [(0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1)]]
+          mdeltas = map (`elem` ps) deltas
